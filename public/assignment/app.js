3dd647eb289d119b5d765f16bd76d4670db2fb90
Sushant Pritmani <whisprrind.twister@gmail.com>
2017-09-09 17:35:54 -0400

    3.5 : Routing

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -1,2 +1,14 @@
 // App name and second argument is dependencies / config fn
-angular.module("WamApp",[])
+(function () {
+    angular.module('WamApp', ['ngRoute'])
+        .config(configuration);
+
+    // what are you configuring + Provider (always ends with provider)
+    function configuration($routeProvider) {
+        $routeProvider
+            .when('/home', {
+                templateUrl: 'home.html'
+            })
+    }
+
+})();
