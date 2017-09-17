3265d3ddfacd1027daa5f55aff4ac4636b969dbc
Sushant Pritmani <whisprrind.twister@gmail.com>
2017-09-16 23:29:15 -0400

    Login Controller and View : Completed

--- public/assignment/views/user/controllers/login.controller.view.js
+++ public/assignment/views/user/controllers/login.controller.view.js
@@ -3,36 +3,28 @@
         .module("WamApp")
         .controller("loginController", loginController);
 
-    function loginController($location) {
+    function loginController($location, userService, $rootScope) {
 
-        var users = [
-            {_id: "123", username: "alice", password: "alice", firstName: "Alice", lastName: "Wonder"},
-            {_id: "234", username: "bob", password: "bob", firstName: "Bob", lastName: "Marley"},
-            {_id: "345", username: "charly", password: "charly", firstName: "Charly", lastName: "Garcia"},
-            {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose", lastName: "Annunzi"}
-        ];
+
         var model = this;
         // Event Handlers
         model.login = login;
 
         // Functions to implement
+        function init() {
+            $rootScope.title = "Login";
+        }
 
-        function login (username, password) {
-            var found = null;
-            for (var u in users) {
-                var user = users[u]
-                if (user.username === username && user.password === password) {
-                    found = user
-                }
+        init();
 
-
-            }
+        function login(username, password) {
+            var found = userService.findUserByCredentials(username, password)
 
             if (found !== null) {
                 // $scope.message = "Welcome " + username
                 $location.url("/user/" + found._id)
             } else {
-                model.message = "User " + username + " not found..."
+                model.errorMessage = "Wrong username or Password.."
             }
         }
     }
